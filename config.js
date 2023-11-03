407f5a04c336575b3c94e85e667f035d3e2f33ad
Donghyun Lee <leedonghyun022@gmail.com>
2023-11-03 12:57:12 +0100

    feat: typescript-eslint 버전 업, 플러그인 추천 추가

--- config.js
+++ config.js
@@ -1,14 +1,20 @@
 module.exports = {
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
     "plugin:react/jsx-runtime",
     "prettier",
   ],
   parser: "@typescript-eslint/parser",
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
   settings: { react: { version: "detect" } },
   plugins: ["simple-import-sort"],
   rules: {
