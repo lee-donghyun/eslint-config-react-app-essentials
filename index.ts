04caceffcaf0607571982b3c7558773a07291bb7
Donghyun Lee <leedonghyun022@gmail.com>
2025-12-27 16:50:33 +0900

    Remove baseUrl option from tsconfig.json to streamline module resolution

--- index.ts
+++ index.ts
@@ -1,50 +1,27 @@
+import { defineConfig, Config } from "eslint/config";
 import eslint from "@eslint/js";
-import tseslint, { ConfigArray } from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
+import tseslint from "typescript-eslint";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 import reactHooks from "eslint-plugin-react-hooks";
-import reactPlugin from "eslint-plugin-react";
+import react from "eslint-plugin-react";
 import perfectionist from "eslint-plugin-perfectionist";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
-type Params = {
-  tsconfigRootDir: string;
-  extends?: ConfigArray;
-  scope: ConfigArray[number]["files"];
-};
+const config = defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  jsxA11y.flatConfigs.recommended,
+  reactHooks.configs.flat["recommended-latest"],
+  react.configs.flat.recommended,
+  react.configs.flat["jsx-runtime"],
+  perfectionist.configs?.["recommended-natural"] as Config,
+  eslintConfigPrettier,
+  {
+    rules: {
+      "react/jsx-curly-brace-presence": ["warn", "never"],
+    },
+  }
+);
 
-export const defineConfig = ({
-  tsconfigRootDir,
-  extends: extendConfigs = [],
-  scope,
-}: Params): ConfigArray => {
-  return tseslint
-    .config(
-      eslint.configs.recommended,
-      tseslint.configs.recommendedTypeChecked,
-      tseslint.configs.stylisticTypeChecked,
-      jsxA11y.flatConfigs.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactPlugin.configs.flat.recommended,
-      reactPlugin.configs.flat["jsx-runtime"],
-      perfectionist.configs["recommended-alphabetical"],
-      eslintConfigPrettier,
-      {
-        rules: {
-          "react/jsx-curly-brace-presence": ["warn", "never"],
-          "react/prop-types": "off",
-          "perfectionist/sort-imports": ["error", { tsconfigRootDir }],
-        },
-        languageOptions: {
-          parserOptions: {
-            projectService: true,
-            tsconfigRootDir,
-          },
-        },
-        settings: {
-          react: { version: "detect" },
-        },
-      },
-      ...extendConfigs
-    )
-    .map((config) => ({ ...config, files: scope }));
-};
+export default config;
